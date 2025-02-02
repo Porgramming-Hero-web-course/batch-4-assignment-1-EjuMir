6c9d8f16c472006d84f71db8f08e8c0f4f8a6d0b
Mir Eju <109233172+EjuMir@users.noreply.github.com>
2025-02-02 15:34:33 +0600

    Update problem-7.ts

--- problem-7.ts
+++ problem-7.ts
@@ -1,15 +1,13 @@
 //problem 7
-
 {
-
     class Car {
         constructor(public name: string, public model: string, public year: number){}
         getCarAge(){
             const currentYear = new Date().getFullYear()
-            return console.log(currentYear - this.year);
+            return (currentYear - this.year);
         }
     }
     
-    const car = new Car("Honda", "Civic", 2018);
-    car.getCarAge();
+    // const car = new Car("Honda", "Civic", 2018);
+    // console.log(car.getCarAge());
 }
