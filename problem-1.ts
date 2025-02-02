d3d20bb2286c782bf7ce24cb2ef5efa308254388
Mir Eju <109233172+EjuMir@users.noreply.github.com>
2025-02-02 15:28:20 +0600

    Update problem-1.ts

--- problem-1.ts
+++ problem-1.ts
@@ -1,8 +1,9 @@
 //problem 1
-{
-let sum: number = 0;
-const sumArray = (param: number[]): number => {
-   return param.forEach((val: number) => sum += val);
-}
-sumArray([1, 2, 3, 4, 5]);
-}
+ {
+        let sum: number = 0;
+        const sumArray = (param: number[]): number => {
+           param.forEach((val: number) => sum += val);
+           return sum
+        }
+        // sumArray([1, 2, 3, 4, 5]);
+ }  
